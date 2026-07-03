9e1e234185154bb1cfb36f157c3d49f5639e6bf0
infinnitor <infinnitor@gmail.com>
2026-07-03 22:30:49 +0100

    change resolution

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export var walk_speed = 5.0
-@export var gravity = 9.8
+@export var gravity = 20.8
 @export var jump_strength = 7.0
 
 @export var mouse_sensitivity = 0.002
@@ -13,6 +13,7 @@ extends CharacterBody3D
 func _ready():
 	# This hides our mouse cursor and keeps it locked to the game window.
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -27,12 +28,12 @@ func _input(event):
 		head.rotation.x = clamp(head.rotation.x, -PI/2, PI/2)
 
 func _physics_process(delta):
-	# First, apply gravity so we don't float.
+	# First, apply gravity so wew  don't float.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	else:
 		# Jumping 
-		velocity.y += float(Input.is_action_just_pressed("jump")) * jump_strength
+		velocity.y += float(Input.is_action_pressed("jump")) * jump_strength
 	
 	# Get our WASD input.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
